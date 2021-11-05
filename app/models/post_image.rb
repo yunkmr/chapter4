5708953ac4273d851974ef8ed730b74c7355843d
EC2 Default User <ec2-user@ip-172-31-89-202.ec2.internal>
2021-11-05 00:13:20 +0000

    [Finsih] 4-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,12 @@
 class PostImage < ApplicationRecord
 
   belongs_to :user
-  attachment :image # ここを追加（_idは含めません）
+  attachment :image
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
 end
